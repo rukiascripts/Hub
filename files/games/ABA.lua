bd9a8d43153adcf9ca4b95cd9cb93f6a12944d5a
jacob <luaujacob@gmail.com>
2026-06-18 01:15:40 -0600

    Update ABA.lua

--- files/games/ABA.lua
+++ files/games/ABA.lua
@@ -188,20 +188,17 @@ local function WatchCutter(character: Model, cutter: GuiObject, goal: GuiObject,
 		if (lastX and (lastX :: number) <= goalX and cutterX > goalX) then
 			task.wait(math.random() * 0.04);
 
-				local Mouse = Players.LocalPlayer:GetMouse();
-
-local MouseHit = Mouse.Hit;
-		local args = {
-	"M1",
-	{
-		air = false,
-		skeyreal = false,
-		skeydown = true,
-		mousehit = MouseHit,
-		md = vector.zero
-	}
-}
-game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Input"):FireServer(unpack(args))
+			local args = {
+				"M1",
+				{
+					air = false,
+					skeyreal = false,
+					skeydown = true,
+					mousehit = CFrame.new(2282.69091796875, 957.7990112304688, 113.0128402709961, 0.1719614416360855, -0.44762375950813293, 0.877531886100769, -0, 0.8908015489578247, 0.45439255237579346, -0.9851036667823792, -0.07813800126314163, 0.15318351984024048),
+					md = vector.zero
+				}
+			}
+			game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Input"):FireServer(unpack(args))
 
 
 			if (connection) then
